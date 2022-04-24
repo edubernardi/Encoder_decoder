Bug fixes, size change calculation

hamming.py
@@ -26,7 +26,6 @@ def encode(data):
         else:
             result[i] = codewords[1][i - 7]
         i += 1
-
     return result
 
 def decode(data):
@@ -65,11 +64,11 @@ def decode(data):
                 if flags[2]:
                     codewords[i][2] = (codewords[i][2] + 1) % 2
                 else:
-                    codewords[i][1] = (codewords[i][2] + 1) % 2
+                    codewords[i][1] = (codewords[i][1] + 1) % 2
             else:
-                codewords[i][0] = (codewords[i][2] + 1) % 2
+                codewords[i][0] = (codewords[i][0] + 1) % 2
         elif flags[1] or flags[2]:
-            codewords[i][3] = (codewords[i][2] + 1) % 2
+            codewords[i][3] = (codewords[i][3] + 1) % 2
         i += 1
 
     result = np.ndarray(int((4 * len(bits))/7), int)

util.py
@@ -144,10 +144,11 @@ def encode_noise_control(input_file, output_file):
         loaded_byte = f.read(1)
         if len(loaded_byte) < 1:
             end_of_file = True
-        codeword = hamming.encode(loaded_byte)
-        for number in codeword:
-            buffer[j] = number
-            j += 1
+        if not end_of_file:
+            codeword = hamming.encode(loaded_byte)
+            for number in codeword:
+                buffer[j] = number
+                j += 1
         if end_of_file or j % 8 == 0:
             codewords = bytearray(np.packbits(buffer[0: j]))
             for byte in codewords:
@@ -191,10 +192,10 @@ def decode_noise_control(input_file, output_file):
     buffer = np.empty(800000, int)
     j = 0
     while not end_of_file and verified_headers:
-        loaded_bytes = f.read(7)
+        loaded_bytes = bytearray(f.read(7))
         if len(loaded_bytes) < 7:
             end_of_file = True
-        codeword = hamming.decode(bytearray(loaded_bytes))
+        codeword = hamming.decode(loaded_bytes)
         for number in codeword:
             buffer[j] = number
             j += 1